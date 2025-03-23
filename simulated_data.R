bf9a83f0696d5628d277cc9ee2805ec2f59c7978
Sophievcy <sophievc@shaw.ca>
2025-03-22 19:55:49 -0700

    scripts for calculating species richness, abundance, and eveness

---
+++ simulated_data.R
@@ -0,0 +1,27 @@
+#simulated data test
+
+#load packages
+library(vegan)
+library(tidyverse)
+library(dplyr)
+
+#load data
+inverts_simulated <- read_csv("C:/Users/sophi/OneDrive/Desktop/BIOL404 - invert counts - simulated_data.csv")
+
+#calculate species richness using shannon diversity index
+
+# Select only the species columns
+species_data <- inverts_simulated[, 3:7]
+
+# Calculate Shannon Diversity Index for each row
+inverts_simulated$shannon_index <- diversity(species_data, index = "shannon")
+
+inverts_simulated$species_richness <- specnumber(species_data)
+
+#calculate Pielous J for species eveness
+inverts_simulated$species_evenness <- inverts_simulated$shannon_index / log(inverts_simulated$species_richness)
+
+#calculate species abundance
+inverts_simulated$total_abundance <- rowSums(species_data)
+                            
+                            
