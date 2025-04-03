fdaf08a66b28daeb8c35b9f86bf923d84483592c
Sophievcy <sophievc@shaw.ca>
2025-04-02 17:08:29 -0700

    removed titles

--- scripts/graphs.R
+++ scripts/graphs.R
@@ -6,7 +6,7 @@ ggplot(inverts, aes(x = d_from_path_m, y = species_evenness)) +
   geom_point(alpha = 0.5) +
   geom_smooth(method = "glm", method.args = list(family = "poisson"), color = "blue") +
   labs(x = "Distance from Path (m)", y = "Species Evenness",
-       title = "Effect of Distance from Path on Species Evenness") +
+       ) +
   theme_minimal()
 
 
@@ -15,7 +15,7 @@ ggplot(inverts, aes(x = d_from_path_m, y = species_richness)) +
   geom_point(alpha = 0.5) +
   geom_smooth(method = "glm", method.args = list(family = "poisson"), color = "blue") +
   labs(x = "Distance from Path (m)", y = "Species Richness",
-       title = "Effect of Distance from Path on Species Richness") +
+       ) +
   theme_minimal()
 
 #plot for species abundance
@@ -23,5 +23,5 @@ ggplot(inverts, aes(x = d_from_path_m, y = total_abundance)) +
   geom_point(alpha = 0.5) +
   geom_smooth(method = "glm", method.args = list(family = "poisson"), color = "blue") +
   labs(x = "Distance from Path (m)", y = "Species Abundance",
-       title = "Effect of Distance from Path on Species Abundance") +
+       ) +
   theme_minimal()
